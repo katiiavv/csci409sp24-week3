56e679bf8888d8cb44b0fbe99bbbb127ca80a2a2
Kathleen Avvento <keavvento@coastal.edu>
2024-02-21 14:35:48 -0500

    Created Runways model

--- api/core/models.py
+++ api/core/models.py
@@ -11,3 +11,30 @@ class Airport(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Airline(models.Model):
+    name = models.CharField(max_length=255)
+    airline_code = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name
+    
+class Runway(models.Model):
+
+    RUNWAY_DESTINATION_CHOICES = [
+        ('L', 'Left'),
+        ('C', 'Center'),
+        ('R', 'Right'),
+        ('N', 'None'),
+    ]
+
+
+    runway_number = models.IntegerField()
+    runway_designation = models.CharField(max_length=1, choices=RUNWAY_DESTINATION_CHOICES)
+    length = models.IntegerField()
+    width = models.IntegerField()
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.runway_number}{self.runway_designation}"
