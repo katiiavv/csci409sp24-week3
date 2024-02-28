1983cd0ffc3242f76431ec9f354582a9db8c3afa
Kathleen Avvento <keavvento@coastal.edu>
2024-02-28 14:16:38 -0500

    Made Database Modifications3

--- api/core/models.py
+++ api/core/models.py
@@ -34,7 +34,7 @@ class Runway(models.Model):
     runway_designation = models.CharField(max_length=1, choices=RUNWAY_DESTINATION_CHOICES)
     length = models.IntegerField()
     width = models.IntegerField()
-    airport = models.ForeignKey(Airport, on_delete=models.CASCADE, relate_name='runways')
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='runways')
     
     
     def __str__(self):
